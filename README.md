531ffb384264adfbd78bfa9e59b7fb9713c5602d
amoshochman <amoshochman1982@gmail.com>
2023-06-13 15:17:06 +0300

    using 5-list

--- README.md
+++ README.md
@@ -16,3 +16,11 @@ The code is written allowing for concurrency and expecting more POSTs than GETs.
 
 **Instructions to run**
 You can either run gunicorn_start.sh or through the Dockerfile (don't forget to redirect to port 5000. That is, run for example "docker build -t freq ." and then "docker run -p 5000:5000 freq")
+
+Implementation note:
+For retrieving the top words, we use an additional list of size 5. 
+This makes the POSTs slower by a constant time -as the size of this additional list is constant-. 
+That is, we add the overhead of inserting to and removing from this additional list with the top words. 
+On other hand, this way we make the GETs faster, as we'll always just retrieve the data from the short list, instead
+of iterating over the whole data, which appears to be not bounded.
+Being that the POSTs are supposed to be much more than the GETs, we'd need more information in order to take a wiser decision.
--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, request, jsonify
 from collections import Counter
 
 app = Flask(__name__)
-words_counter = Counter()
+counter = Counter()
+top_words = []
 lock = threading.Lock()
 
 MOST_COMMON = 5
@@ -12,10 +13,36 @@ MINIMUM = 'minimum'
 MEDIAN = 'median'
 
 
+def update_top_words(new_words):
+    if top_words:
+        smaller_top_count = min(counter[word] for word in top_words)
+    else:
+        new_word = new_words[0]
+        smaller_top_count = counter[new_word]
+        top_words.append(new_word)
+        new_words = new_words[1:]
+
+    for new_word in new_words:
+        if new_word in top_words:
+            continue
+        if len(top_words) < MOST_COMMON:
+            top_words.append(new_word)
+            smaller_top_count = min(counter[top_word] for top_word in top_words)
+            continue
+        if counter[new_word] <= smaller_top_count:
+            continue
+        smaller_top_word = next(top_word for top_word in top_words if counter[top_word] == smaller_top_count)
+        smaller_index = top_words.index(smaller_top_word)
+        top_words[smaller_index] = new_word
+
+
 def process_words_async(data):
     with lock:
-        words = [elem.strip() for elem in data.split(",")]
-        words_counter.update(words)
+        new_words = [elem.strip() for elem in data.split(",")]
+        if not new_words:
+            return
+        counter.update(new_words)
+        update_top_words(new_words)
 
 
 @app.route('/words', methods=['POST'])
@@ -26,15 +53,15 @@ def process_words():
         return jsonify("Please provide a valid UTF-8 encoded string", 400)
     thread = threading.Thread(target=process_words_async, args=(data,))
     thread.start()
+    #process_words_async(data)
     return jsonify("processing started")
 
 
 @app.route('/stats', methods=['GET'])
 def get_stats():
     with lock:
-        top_words = words_counter.most_common(MOST_COMMON)
-        min_frequency = min(words_counter.values(), default=None)
-        frequencies = sorted(words_counter.values())
+        min_frequency = min(counter.values(), default=None)
+        frequencies = sorted(counter.values())
         n = len(frequencies)
         if n == 0:
             median_frequency = None
@@ -44,10 +71,9 @@ def get_stats():
             median_frequency = frequencies[n // 2]
 
     dict_response = {
-        TOP: [' '.join([str(elem) for elem in my_tuple]) for my_tuple in top_words],
+        TOP: [word + " " + str(counter[word]) for word in top_words],
         MINIMUM: min_frequency,
         MEDIAN: median_frequency
     }
 
     return jsonify(dict_response)
-
